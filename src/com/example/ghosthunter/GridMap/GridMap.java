0870e71848ba60ea4f2e112f7f2f2529084f2928
AndrewCoffee <acoffee712@gmail.com>
2014-11-19 01:01:10 -0500

    GridMap
    Added collision detection and overlap detection. Syntax is
    straightforward.

--- src/com/example/ghosthunter/GridMap/GridMap.java
+++ src/com/example/ghosthunter/GridMap/GridMap.java
@@ -49,4 +49,37 @@ public class GridMap {
 		Environment[] himmler = (Environment[])hitler.toArray();
 		return himmler;
 	}
+	
+	public boolean overlap(Character A, Character B){ //Fairly simple collision detection
+		return A.getRect().intersect(B.getRect());
+	}
+	
+	/*
+	 * Right now this checks against all other sprites.
+	 * This is quite inefficient, but this is the quickest way to code.
+	 * I'm considering making some TreeSets of all the Environment objects,
+	 * since the Character list likely won't go beyond 20 or so, which isn't
+	 * too computationally intensive.
+	 * The TreeSet route would make use of two sorted lists, with all ghosts
+	 * and characters in it. Then it would check against nearby items in the list.
+	 */
+	public boolean moveAllowed(Character A, int direction){
+		int xoff = (int)Math.cos(Math.PI * direction); //calculate the x offset
+		int yoff = (int)Math.sin(Math.PI * direction); // calculate the y offset
+		for(Character ch : this.chars){ //check for all of the characters
+			if(A.getPos()[0]+A.getLen()[0]+xoff == ch.getPos()[0]+ch.getLen()[0]){
+				if(A.getPos()[1]+A.getLen()[1]+yoff == ch.getPos()[1]+ch.getLen()[1])
+					return false;
+			}
+		}
+		for(Environment en : this.envis){ //check for all of the nonwalkable enviro's
+			if(!en.getWalkable()){
+				if(A.getPos()[0]+A.getLen()[0]+xoff == en.getPos()[0]+en.getLen()[0]){
+					if(A.getPos()[1]+A.getLen()[1]+yoff == en.getPos()[1]+en.getLen()[1])
+						return false;
+				}
+			}
+		}
+		return true;
+	}
 }

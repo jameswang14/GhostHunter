c5eeb63d320cbb12a51f85a54db3ef33ad235515
AndrewCoffee <acoffee712@gmail.com>
2014-11-18 19:14:17 -0500

    Fixed BasicGhost
    Fixed the pathfinding.

--- src/com/example/ghosthunter/Ghost/BasicGhost.java
+++ src/com/example/ghosthunter/Ghost/BasicGhost.java
@@ -4,7 +4,7 @@ import android.graphics.drawable.BitmapDrawable;
 import com.example.ghosthunter.Character.Character;
 
 
-/* The stand ghost right here.
+/* The standard ghost right here.
  * It chases after you and tries to eat you.
  * Easy to kill, easy to avoid.
  * Damage: 10. Speed: 3. Armor: 0. HP: 15. Ignores walls.
@@ -14,12 +14,18 @@ import com.example.ghosthunter.Character.Character;
 
 public class BasicGhost extends Ghost {
 	
-	public BasicGhost(int[] pos, BitmapDrawable[] images, int damage, int speed){
-		super(pos, new int[]{2,2}, 15, images, true, damage, 3, 0);
+	public BasicGhost(int[] pos, BitmapDrawable[] images, int speed){
+		super(pos, new int[]{2,2}, 15, images, true, 10, 3, 0);
 	}
 	
-	public int getNextDirection(Character target){
-		return Math.max(target.getPos()[0]-super.getPos()[0], target.getPos()[1]-super.getPos()[1]); //return the bigger difference
+	//0-E,1-N,2-W,3-S
+	public int getNextDirection(Character target){ //to be fixed
+		if(Math.abs(target.getPos()[0]-super.getPos()[0]) < Math.abs(target.getPos()[1]-super.getPos()[1])){
+			if(target.getPos()[0]-super.getPos()[0] < 0) return 0;
+				return 2;
+		}
+		if(target.getPos()[1]-super.getPos()[1] > 0) return 1;
+			return 3;
 	}
 
 	public BitmapDrawable draw(){ //currently does NOT use status effects
@@ -33,8 +39,5 @@ public class BasicGhost extends Ghost {
 		target.takeDamage(deal);
 		return deal; //in case we want to display the damage for whatever reason
 	}
-	
-	public void tickStatus(){
 		
-	}
 }

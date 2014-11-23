77c6d3d3019e0cc72b8f78efbd3e9fd0967d1f40
raubrey2014 <rma7qb@virginia.edu>
2014-11-23 17:32:47 -0500

    Testing Player

--- src/com/example/ghosthunter/GamePage.java
+++ src/com/example/ghosthunter/GamePage.java
@@ -1,5 +1,6 @@
 package com.example.ghosthunter;
 
+import com.example.ghosthunter.Character.Player;
 import com.example.ghosthunter.Ghost.BasicGhost;
 import com.example.ghosthunter.GridMap.GridMap;
 
@@ -24,22 +25,38 @@ public class GamePage extends Activity {
 	DrawingPanel d;
 	int xcor = 0;
 	int ycor = 0;
+	int playerX = 250;
+	int playerY = 250;
+	
 	Bitmap b;
+	Bitmap b2;
 	BasicGhost ghost;
 	BasicGhost ghost1;
+	Player p;
 	boolean running = false;
 	int[] a;
+	int[] a2;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		
 		super.onCreate(savedInstanceState);
 		d = new DrawingPanel(this);
 		setContentView(d);
+		b2 = BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher);
 		b=BitmapFactory.decodeResource(getResources(), R.drawable.ghosticon);
 		a = new int[2];
+		a2 = new int[2];
+		a2[0] = 100;
+		a2[1] = 200;
+		
 		a[0]=0;
 		a[1]=0;
 		Bitmap[] images = new Bitmap[2];
+		Bitmap[] images2 = new Bitmap[2];
+		images2[0] = b2;
+		images2[1] = b2;
+		
 		images[1] = b;
 		images[0] = b;
 		int[] location = new int[2];
@@ -49,6 +66,10 @@ public class GamePage extends Activity {
 		GridMap grid = new GridMap(0,0,0);
 		ghost = new BasicGhost(a,images,grid,this);
 		ghost1 = new BasicGhost(location,images,grid,this);
+		
+		//int hp, Bitmap[] images, int armor, GridMap grid,Context context
+		p = new Player(50, images2, 10, grid, this);
+		p.setPos(a2);
 		//grid.addCharacter(ghost);
 		
 
@@ -95,11 +116,13 @@ public class GamePage extends Activity {
 		}
 		public void onDraw(Canvas canvas)
 		{
-			
+			Paint paint2 = new Paint(Paint.ANTI_ALIAS_FLAG);
+			paint2.setColor(Color.GREEN);
 			canvas.drawARGB(255, 0, 0, 0);
 			xcor++;
 			ycor++;
 			canvas.drawBitmap(ghost.draw(), xcor,ycor, paint);
+			canvas.drawBitmap(p.draw(), playerX,playerY , paint2);
 		
 
 		}
